e36bd106d3116554d82a6a1d03ed5c70a925529b
Sankha Cooray <bsc2fast@hotmail.com>
2023-09-30 18:40:00 +0800

    app updated

--- app/ServiceWorker.js
+++ app/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-stakco-app-unity-1.0";
 const contentToCache = [
     "Build/WebGL.loader.js",
-    "Build/WebGL.framework.js.br",
-    "Build/WebGL.data.br",
-    "Build/WebGL.wasm.br",
+    "Build/WebGL.framework.js",
+    "Build/WebGL.data",
+    "Build/WebGL.wasm",
     "TemplateData/style.css"
 
 ];
