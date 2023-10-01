4f574d0794f084b5cc72a2cf85ae44c63b34b120
Sankha Cooray <bsc2fast@hotmail.com>
2023-10-01 18:30:20 +0800

    add solve button and feature to app

--- app/ServiceWorker.js
+++ app/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-stakco-app-unity-1.0";
+const cacheName = "Stakcos-Stakco-1.0.2";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js",
