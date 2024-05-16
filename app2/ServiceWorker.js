90bf7861fd2bbc1250666200efe0a630897ac9e5
Sankha Cooray <bsc2fast@hotmail.com>
2024-05-16 17:18:43 +0800

    switch layer button size changed

--- app2/ServiceWorker.js
+++ app2/ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "Stakcos-Stakco-1.0.13";
 const contentToCache = [
     "Build/APP_WebGL.loader.js",
     "Build/86d19f814749cb40fe725857947162c1.js",
-    "Build/e11c0283b9bdc205a812a283ad49ad4c.data",
+    "Build/3cba557853072c6e11d5bad7e3835822.data",
     "Build/1c881c0cb95f95ae75b5c3077b5eaf71.wasm",
     "TemplateData/style.css"
 
