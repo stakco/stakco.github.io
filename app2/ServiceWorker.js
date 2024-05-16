a4bb1f82fb238fe20bfe690b72071799d687adb0
Sankha Cooray <bsc2fast@hotmail.com>
2024-05-16 18:54:54 +0800

    app2 test

--- app2/ServiceWorker.js
+++ app2/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Stakcos-Stakco-1.0.13";
 const contentToCache = [
     "Build/APP_WebGL.loader.js",
-    "Build/86d19f814749cb40fe725857947162c1.js",
-    "Build/dbdf1c28ecec04dc585548102c8ae3a9.data",
-    "Build/69aa764d2fdf651a4dea4b30bb1f62e6.wasm",
+    "Build/38d050dfd44a62e9850ab33f0f0d068f.js",
+    "Build/0fc97da14eff5262e5de3912ef2162cf.data",
+    "Build/a25d1717fa1200ea3b52e0dff3de9374.wasm",
     "TemplateData/style.css"
 
 ];
