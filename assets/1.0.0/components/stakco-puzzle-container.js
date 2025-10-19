8a6b4ac80afdd4c331c0910030f8e27de293cd31
Sankha Cooray <bsc2fast@hotmail.com>
2025-10-19 23:12:25 +0800

    Deploy src to assets/1.0.0

--- assets/1.0.0/components/stakco-puzzle-container.js
+++ assets/1.0.0/components/stakco-puzzle-container.js
@@ -392,7 +392,7 @@ class StakcoPuzzleContainer extends HTMLElement {
 
         if (aligned) {
             this.isSolved = true;
-            animateToSolution();
+            this.animateToSolution();
             this.dispatchEvent(new CustomEvent('puzzle-solved', {
                 detail: { 
                     rotations: this.puzzleLayers,
